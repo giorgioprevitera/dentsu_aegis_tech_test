7238fa97293ba92a0554d009245c209daf370fbf
Giorgio Previtera <giorgioprevitera@gmail.com>
2018-04-25 00:28:42 +0100

    Adding basic logging

--- code/index.js
+++ code/index.js
@@ -2,9 +2,11 @@ const os = require('os');
 var http = require('http');
 
 function onReq(req, res){
+  console.log(`Received request from ${req.url}`);
   res.writeHead(200, {"Content-Type": "text/plain"});
   res.write(`Hello World from ${os.hostname()}!`);
   res.end();
 }
 
 http.createServer(onReq).listen(8080);
+console.log("Server has started.");
