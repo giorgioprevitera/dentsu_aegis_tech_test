62494c7570105c69ce79220d8459de739474742f
Giorgio Previtera <giorgioprevitera@gmail.com>
2018-04-25 00:30:39 +0100

    Typo

--- code/index.js
+++ code/index.js
@@ -2,7 +2,7 @@ const os = require('os');
 var http = require('http');
 
 function onReq(req, res){
-  console.log(`Received request from ${req.url}`);
+  console.log(`Received request for ${req.url}`);
   res.writeHead(200, {"Content-Type": "text/plain"});
   res.write(`Hello World from ${os.hostname()}!`);
   res.end();
